475cdbfb083587ba7f4b053d6533922717be4c78
kvm <koloev_vis@mail.ru>
2021-10-15 20:45:30 +0300

    Создал модель пользователя приложения

---
+++ users/models.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+GENDER = [
+        ('man', 'Мужской'),
+        ('woman', 'Женский'),
+    ]
+
+
+class CustomUserManager(BaseUserManager):
+    
+    def create_user(self, first_name, last_name, gender, email, password=None):
+        """Создает нового пользователя приложения с сохранением данных об email, имени, фамилии и поле"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            gender=gender,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, gender, email, password=None):   
+        """Создает суперпользователя приложения с сохранением данных об email, имени, фамилии и поле"""
+        user = self.create_user(
+            first_name,
+            last_name,
+            gender,
+            email,
+            password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+
+    first_name = models.CharField(verbose_name="First name", max_length=50)
+    last_name = models.CharField(verbose_name="Last name",  max_length=50)
+    user_picture = models.ImageField(verbose_name = "User picture", upload_to='user_pictures')
+    gender = models.CharField (verbose_name="Gender", max_length=8, choices=GENDER) 
+    email = models.EmailField(verbose_name="Email", max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'gender']
+    
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
